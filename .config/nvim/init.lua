d54f8ff9bbe7c1f9f7adde406769f44ca4826aad
Daniil <dadyarri@gmail.com>
2021-05-26 01:42:43 +0300

    Add autopairs plugin; clean up imports; add settings import.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,14 @@
 -- Import modules
 require('plugins')
 require('keys')
+
+-- Configure plugins
 require('nv-compe')
 require('nv-comment')
+require('nv-autopairs')
+
+-- Enable language servers
+require('lsp.lua-ls')
+require('lsp.python-ls')
+require('settings')
+
