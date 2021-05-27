6a973a4ab62b6ee493cf298643934580512a751d
Daniil <dadyarri@gmail.com>
2021-05-27 21:00:25 +0300

    Split settings to scopes (thanks to @fdioguardi)

--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -1,29 +1,26 @@
-vim.o.relativenumber = true
-vim.o.number = true
-vim.o.autoindent = true
-vim.o.tabstop = 4
-vim.o.shiftwidth = 4
-vim.o.smarttab = true
-vim.o.softtabstop = 4
+-- Buffer-related options
+vim.bo.autoindent = true
+vim.bo.fileencoding = "utf-8"
+vim.bo.shiftwidth = 4
+vim.bo.softtabstop = 4
+vim.bo.tabstop = 4
 
-vim.o.mouse = "a"
-vim.o.encoding = "utf-8"
-
-vim.o.cursorline = true
-
+-- Global options
 vim.o.background = "dark"
+vim.o.backup = false
+vim.o.clipboard = "unnamedplus"
 vim.o.cmdheight = 2
+vim.o.encoding = "utf-8"
+vim.o.ignorecase = true
+vim.o.mouse = "a"
+vim.o.showmode = false
+vim.o.smartcase = true
+vim.o.smarttab = true
 vim.o.splitbelow = true
 vim.o.splitright = true
-vim.o.t_Co = "256"
-vim.o.fileencoding = "utf-8"
-vim.o.backspace = "indent,eol,start"
-vim.o.incsearch = true
-vim.o.ignorecase = true
-vim.o.smartcase = true
-vim.o.backup = false
 vim.o.writebackup = false
-vim.o.laststatus = 2
-vim.o.showmode = false
-vim.o.clipboard = "unnamedplus"
 
+-- Window-related options
+vim.wo.cursorline = true
+vim.wo.number = true
+vim.wo.relativenumber = true
