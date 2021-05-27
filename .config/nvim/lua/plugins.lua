e63bf88346cbd427bc096bbd7019f9e5db75fcb6
Daniil <dadyarri@gmail.com>
2021-05-27 23:38:24 +0300

    Add some plugins

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -13,3 +13,17 @@ paq {'hrsh7th/nvim-compe'} -- LSP
 paq {'hrsh7th/vim-vsnip'} -- Snippets
 paq {'terrortylor/nvim-comment'} -- Auto comment selected
 paq {'windwp/nvim-autopairs'} -- Auto close paired brackets
+paq {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'} -- Improved code highlighting
+paq {'kyazdani42/nvim-web-devicons'} -- Icons
+paq {'glepnir/galaxyline.nvim'} -- Status line
+paq {'nvim-lua/popup.nvim'}
+paq {'nvim-lua/plenary.nvim'}
+paq {'nvim-telescope/telescope.nvim'}
+paq {'nvim-telescope/telescope-media-files.nvim'}
+paq {'glepnir/lspsaga.nvim'}
+paq {'onsails/lspkind-nvim'}
+paq {'kosayoda/nvim-lightbulb'}
+paq {'mfussenegger/nvim-jdtls'}
+paq {'mfussenegger/nvim-dap'}
+paq {'wojciechkepka/vim-github-dark'}
+
