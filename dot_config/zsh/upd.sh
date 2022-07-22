6a913120a87bbf0d1202734512780d5b98b7ab43
Daniil <dadyarri@gmail.com>
2022-07-22 16:45:48 +0300

    add help message to upd script

--- dot_config/zsh/upd.sh
+++ dot_config/zsh/upd.sh
@@ -1,21 +1,37 @@
+_upd_help() {
+    echo "Little wrapper to update entire system."
+    echo
+    echo "Syntax: upd"
+}
+
 upd() {
-    echo "Updating system started..."
-    echo "Password required"
-    echo
-    echo -n "Enter sudo password: "
-    read -r -s password
-    echo
-    echo "Updating packages..."
-    echo $password | sudo -S dnf update -y
-    echo "Packages updated."
-    echo
-    echo "Removing orphans..."
-    echo $password | sudo -S dnf autoremove -y
-    echo "Orphans removed."
-    echo
-    echo "Updating flatpak apps..."
-    flatpak update
-    echo "Flatpak apps updated."
-    echo
-    echo "System updated."
+    for i in "$@"; do
+        case $i in
+            -h|--help)
+              _upd_help
+              shift
+              ;;
+            *)
+                echo "Updating system started..."
+                echo "Password required"
+                echo
+                echo -n "Enter sudo password: "
+                read -r -s password
+                echo
+                echo "Updating packages..."
+                echo $password | sudo -S dnf update -y
+                echo "Packages updated."
+                echo
+                echo "Removing orphans..."
+                echo $password | sudo -S dnf autoremove -y
+                echo "Orphans removed."
+                echo
+                echo "Updating flatpak apps..."
+                flatpak update
+                echo "Flatpak apps updated."
+                echo
+                echo "System updated."
+                ;;
+        esac
+    done
 }
