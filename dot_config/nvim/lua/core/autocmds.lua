48203263c01fb8914ad3a771eac3bd14a9904ae8
Daniil <dadyarri@gmail.com>
2022-07-23 14:02:15 +0300

    update autocmds

--- dot_config/nvim/lua/core/autocmds.lua
+++ dot_config/nvim/lua/core/autocmds.lua
@@ -43,7 +43,7 @@ augroup('setIndent', { clear = true })
 autocmd('Filetype', {
   group = 'setIndent',
   pattern = { 'xml', 'html', 'xhtml', 'css', 'scss', 'javascript', 'typescript',
-    'yaml', 'lua'
+    'yaml', 'lua', 'sh'
   },
   command = 'setlocal shiftwidth=2 tabstop=2'
 })
