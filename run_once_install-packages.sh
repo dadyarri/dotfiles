6a0366cccbb6744382dda8c8ce394877dda7dd07
Daniil <dadyarri@gmail.com>
2022-07-25 01:36:06 +0300

    add downloading afx; updatind system; rebooting system

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 
+# Copy custome repositories to desired location
 sudo cp repos/* /etc/yum.repos.d
 
-sudo dnf install bat brave-browser code docker-ce docker-ce-cli docker-compose-plugin dotnet exa fd-find fzf git git-delta glow kio-gdrive neovim nodejs notion-app spectacle wezterm xclip zsh -y
+# Install desired packages; Update system
+sudo dnf install bat brave-browser code curl docker-ce docker-ce-cli docker-compose-plugin dotnet exa fd-find fzf git git-delta glow kio-gdrive neovim nodejs notion-app spectacle wezterm xclip zsh -y
+sudo dnf update -y
+
+# Get latest version of afx (package manager for shell)
+kernelName=$(uname -s)
+machine=$(uname -m)
+url="https://github.com/b4b4r07/afx/releases/latest/download/afx_${kernelName,,}_$machine.tar.gz"
+file="afx.tar.gz"
+
+curl -L $url -o $file
+
+sudo tar xzf $file -C /usr/local/bin
+rm -rf $file
+
+# Set zsh as default shell for current user
+sudo chsh -s $(which zsh) $USER
+
+echo "Now you can reboot your system. Do it now? [y/n]"
+read ans
+
+if [[ ($ans -eq "y") || ($ans -eq "yes") ]]; then
+    reboot
+fi
+
