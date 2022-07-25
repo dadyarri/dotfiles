d1521e716563281f7e492d7404f6d938787924c4
Daniil <dadyarri@gmail.com>
2022-07-25 16:26:18 +0300

    add some configurations to wezterm

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,5 +1,12 @@
 local wezterm = require 'wezterm';
+
 return {
   font = wezterm.font("JetBrainsMono Nerd Font Mono"),
+  font_size = 14,
   color_scheme = "OneHalfDark",
+  initial_rows = 20,
+  initial_cols = 100,
+  default_cursor_style = "SteadyBar",
+  term = "wezterm",
+  warn_about_missing_glyphs = true,
 }
