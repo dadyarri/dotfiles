b95a6d3876a0c8bdc70812fa5fa8586420d36d05
Daniil <dadyarri@gmail.com>
2022-07-28 01:20:50 +0300

    update zsh keybindings

--- dot_config/zsh/keybindings.sh
+++ dot_config/zsh/keybindings.sh
@@ -1,3 +1,6 @@
 bindkey  "^[[H"   beginning-of-line
 bindkey  "^[[F"   end-of-line
 bindkey  "^[[3~"  delete-char
+
+bindkey '^[[A' history-substring-search-up
+bindkey '^[[B' history-substring-search-down
