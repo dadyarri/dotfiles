edbc218695e239f12e7e9caa7e76a1cabc344215
Daniil <dadyarri@gmail.com>
2022-07-30 13:47:10 +0300

    remove afx

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -8,20 +8,8 @@ sudo dnf copr enable atim/bottom -y
 sudo dnf install bat brave-browser bottom code curl docker-ce docker-ce-cli docker-compose-plugin dotnet exa fd-find fzf git git-delta glow kio-gdrive neovim nodejs notion-app flameshot wezterm wl-clipboard zsh mosh -y
 sudo dnf update -y
 
-# Get latest version of afx (package manager for shell)
-kernelName=$(uname -s)
-machine=$(uname -m)
-url="https://github.com/b4b4r07/afx/releases/latest/download/afx_${kernelName,,}_$machine.tar.gz"
-file="afx.tar.gz"
-
-curl -L "$url" -o $file
-
-sudo tar xzf $file -C /usr/local/bin
-rm -rf $file
-
 # Set zsh as default shell for current user
 sudo chsh -s "$(which zsh)" "$USER"
-
 
 # Install wezterm's terminfo
 tempfile=$(mktemp) \
