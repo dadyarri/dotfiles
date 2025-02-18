e8ba0048325701c7a2b4aec7b8bb71d63008afc6
Daniil <dadyarri@gmail.com>
2025-02-18 15:32:34 +0300

    remove old dotfiles

--- dot_config/zsh/keybindings.sh
+++
@@ -1,6 +0,0 @@
-bindkey  "^[[H"   beginning-of-line
-bindkey  "^[[F"   end-of-line
-bindkey  "^[[3~"  delete-char
-
-bindkey '^[[A' history-substring-search-up
-bindkey '^[[B' history-substring-search-down
