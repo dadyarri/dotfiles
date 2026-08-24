1c98a1f4f7eab8894bde5bcdb6182548f9be0771
Daniil <dadyarri@gmail.com>
2026-08-24 19:13:18 +0300

    Remove .codex/config.toml Update .config/flameshot/flameshot.ini Update .config/fish/conf.d/70-fnm.fish Update .config/try-rs/config.toml

--- dot_config/private_fish/conf.d/70-fnm.fish
+++ dot_config/private_fish/conf.d/70-fnm.fish
@@ -1,4 +1,4 @@
-set FNM_PATH "/home/dadyarri/.local/share/fnm"
+set FNM_PATH "$HOME/.local/share/fnm"
 if [ -d "$FNM_PATH" ]
   set PATH "$FNM_PATH" $PATH
   fnm env --use-on-cd --shell fish | source
