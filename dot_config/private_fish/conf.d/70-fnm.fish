cd02b56abf399e475c4e6a833d364a150c4cd32c
Daniil <dadyarri@gmail.com>
2026-09-06 22:07:01 +0300

    Update .config/fish/conf.d/70-fnm.fish

--- dot_config/private_fish/conf.d/70-fnm.fish
+++ dot_config/private_fish/conf.d/70-fnm.fish
@@ -2,7 +2,7 @@ status is-interactive; or exit
 
 set -gx FNM_PATH "$HOME/.local/share/fnm"
 
-if test -d "$FNM_PATH"; and type -q fnm
+if test -d "$FNM_PATH"
     set -gx PATH "$FNM_PATH" $PATH
     fnm env --use-on-cd --shell fish | source
 end
